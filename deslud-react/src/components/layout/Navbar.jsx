b235b780b85eec6a757be1ed24e3cf206e4a4246
Digit Tech-DEV <josephstudiotech@gmail.com>
2026-05-12 16:17:25 +0100

    Update Navbar.jsx
    Corrigé

--- deslud-react/src/components/layout/Navbar.jsx
+++ deslud-react/src/components/layout/Navbar.jsx
@@ -116,7 +116,7 @@ export default function Navbar() {
 
           {/* Logo */}
           <button onClick={() => handleNav('#hero')} className="flex items-center gap-3">
-            <div ‎className="w-11 h-11 bg-white rounded-xl flex items-center justify-center text-2xl shadow-glow-blue flex-shrink-0">
+            <div ‎className="w-11 h-11 rounded-xl flex items-center justify-center text-2xl shadow-glow-blue flex-shrink-0">
             <img src="/images/logodeslud.png" alt="DESLUD PLOMBERIE"/>
             </div>
             <div className="leading-none hidden sm:block">
